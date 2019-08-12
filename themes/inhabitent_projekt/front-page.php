260e0383e350f61844409466edde88950cda2e8e
Alex Hing Liu <hingliu6@gmail.com>
2019-08-11 22:53:03 -0700

    Shop Stuff @front-page done - Home @ 33%

--- themes/inhabitent_projekt/front-page.php
+++ themes/inhabitent_projekt/front-page.php
@@ -9,15 +9,51 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-			<?php the_post_thumbnail(); ?>
-			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+		
+		<!-- Hero Section -->
+
+		<section class="front-page-hero">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>\resources\images\logos\inhabitent-logo-full.svg">
+		</section>
+		
+		<!-- Shop Section -->
+
+		<section class="product-info container">
 			
-			<?php endwhile; // End of the loop. ?>
+				<h2>Shop Stuff</h2>
+
+				<!-- Getting the products taxonomy data -->
+
+				<?php
+					$terms = get_terms( array (
+						'taxonomy' => 'product-type',
+						'hide_empty' => 0,
+						) 
+					);
+				if ( ! empty( $terms ) ) :
+				?>
+					<!-- Displaying the products taxonomy data -->
+
+					<div class="product-type-blocks">
+						<?php foreach ( $terms as $term ) :
+						?>
+							<div class="product-type-block-wrapper">
+								<img src="<?php echo get_template_directory_uri() .
+									'/resources/images/product-type-icons/' . $term->slug; ?>.svg"
+									alt="<?php echo $term->name; ?>"/>
+								<p><?php echo $term->description; ?></p>
+								<a href="<?php echo get_term_link( $term ); ?>" class="btn">
+									<?php echo $term->name; ?> Stuff
+								</a>
+							</div>
+						<?php endforeach; ?>
+					</div>
+				<?php endif; ?>
+			</section>
+
 		
 		</main><!-- #main -->
 	</div><!-- #primary -->
 	
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
 
--- themes/inhabitent_projekt/search.php
+++ themes/inhabitent_projekt/search.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<section id="primary" class="content-area-search">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
@@ -19,7 +19,27 @@ get_header(); ?>
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'search' ); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+						<?php if ( 'post' === get_post_type() ) : ?>
+							<div class="entry-meta">
+								<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+							</div><!-- .entry-meta -->
+						<?php endif; ?>
+					</header><!-- .entry-header -->
+
+					<div class="entry-summary">
+						<?php the_excerpt(); ?>
+					</div><!-- .entry-summary -->
+
+					<div class="search-dark-buttons">
+						<a href="<?php the_permalink(); ?>"><button class="dark-button">Read More &rarr;</button>
+					</div>
+
+				</article><!-- #post-## -->
+
 
 			<?php endwhile; ?>
 
